1e3c5e2d2eb4da29f193c93c942e3426cd75ccda
Kekk <danielkekk@gmail.com>
2018-01-07 10:44:21 +0100

    Implementing game over

---
+++ bricksbreaker.js
@@ -0,0 +1,98 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height/2;
+var dx = -1;
+var dy = -2;
+var ballRadius = 10;
+var colorOfBall = "#0095DD";
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+    
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+function randomHexColor() {
+    var n = 6, s = '#';
+    while(n--){
+        s += (Math.random() * 16 | 0).toString(16);
+    }
+    return s;
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = colorOfBall;
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+/*Game loop*/
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    
+    /*Ütközés detektálás: ha elér a canvas széleihez ellentétes irányra váltunk*/
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+        colorOfBall = randomHexColor();
+    }
+    
+    if(y + dy < ballRadius) {
+        dy = -dy;
+        colorOfBall = randomHexColor();
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        /*Ha ütközik a paddle-val visszapattan ha a talajjal ütközik game over*/
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
+    }
+    
+    /*Ha jobb/bal nyíl le van nyomva és nem ért el a széléhez a paddle akkor mozgatjuk*/
+    if(rightPressed === true && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed === true && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
